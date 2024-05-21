08a6e231ba85a08cb83a788feae1780176ee5d72
Brian Haas <bwhaas@gmail.com>
2024-05-20 19:43:52 -0700

    More CORS fixes (#4)
    * More CORS fixes
    
    * fixed typo

--- apigateway.tf
+++ apigateway.tf
@@ -76,7 +76,7 @@ resource "aws_api_gateway_integration" "options_integration" {
   resource_id             = aws_api_gateway_resource.root.id
   http_method             = aws_api_gateway_method.options.http_method
   integration_http_method = "OPTIONS"
-  type                    = "AWS"
+  type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
@@ -123,8 +123,8 @@ resource "aws_lambda_permission" "api_gw" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda_integration
-    #aws_api_gateway_integration.options_integration, # Add this line
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.options_integration 
   ]
 
   rest_api_id = aws_api_gateway_rest_api.hitcounter_api.id
